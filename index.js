9f030bd07e998e67dfd338bc36336eb1eaf7799f
Emreysrcl <2003ysr@gmail.com>
2024-07-11 22:06:52 +0300

    new database

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "worlds",
+  database: "turkey",
   password: "traslan2003trA",
   port: 5432,
 });
